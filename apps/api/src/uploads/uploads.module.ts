b4c6e632d369f0e0ef859099660a3a6d2c071678
yujinhao810 <2499409073@qq.com>
2026-06-29 22:08:17 +0800

    fix:修复后端bug

--- apps/api/src/uploads/uploads.module.ts
+++ apps/api/src/uploads/uploads.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SettingsModule } from '../settings/settings.module';
 import { FileExtractionService } from './file-extraction.service';
 import { UploadsController } from './uploads.controller';
 import { UploadsService } from './uploads.service';
 
 @Module({
+  imports: [SettingsModule],
   controllers: [UploadsController],
   providers: [UploadsService, FileExtractionService],
   exports: [UploadsService],
